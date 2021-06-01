f6ba189c2cdefd4070c50401684eb9d294e127f0
akaur1399 <sonuamandeep99@gmail.com>
2021-06-01 12:09:13 +0530

    commit

---
+++ src/components/Pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../../App.css'
+import Cards from '../Cards'
+import Footer from '../Footer'
+import HomeSection from '../HomeSection'
+
+
+function Home (){
+    return(
+        <>
+        <HomeSection />
+        <Cards />
+        <Footer />
+        </>
+
+
+    )
+}
+
+
+export default Home;
